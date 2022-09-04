d23fd5581b9bcdf3872dfc4c9f3a22ee23801e8d
esrasumer <esrasumer28@gmail.com>
2022-09-04 10:15:54 +0300

    redux

--- src/index.js
+++ src/index.js
@@ -6,9 +6,11 @@ import { Provider } from 'react-redux';
 import store from './store'
 
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
+  </React.StrictMode>
 );
